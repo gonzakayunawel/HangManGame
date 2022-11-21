b04fbca3da93c3ba520503139b0f971fa25194a7
gonzakayunawel <go.cayunao@gmail.com>
2022-11-20 23:55:41 -0300

    Test en Google Colab de funciones

--- game.py
+++ game.py
@@ -7,38 +7,43 @@ def run():
         os.system("clear")
         print('Welcome to the HangManGame')
         print('\nTry to guess the word!')
+        print('\n')
     
-    present()
+    
 
     def startGame():
         print('\n',lines)
         
-    startGame()
-    
-    def changeWord(s):
-        index = word.find(s)
-        new = lines.replace(lines[index], s, 1)
-        return new
+    def changeWord(letter):
+        new = [i for i,c in enumerate(word) if c == letter]
         
     def gaming():
         c=0
         while c <= 2:
-            letter = input('\nEnter a letter...')
-            if letter.upper() in word:
+            present()
+            startGame()
+            letter = input('\nEnter a letter... ')
+            if letter.upper() == 'R':
                 print('\nYeah! Your letter is in the word')
-                present()
-                print(changeWord(letter.upper()))
-                if c == 2:
-                    print('\nYou lose!')
-                    break
             else:
                 print('\nSorry, try again')
-                present()
-                print(changeWord(letter.upper()))
-                if c == 2:
-                    print('\nYou lose!')
-                    break
-            c += 1
+                c+=1
+            
+            
+            # if letter.upper() in word:
+            #     print('\nYeah! Your letter is in the word')
+            #     print(changeWord(letter.upper()))
+            #     if c == 2:
+            #         print('\nYou lose!')
+            #         break
+            # else:
+            #     print('\nSorry, try again')
+            #     present()
+            #     print(changeWord(letter.upper()))
+            #     if c == 2:
+            #         print('\nYou lose!')
+            #         break
+        print('Game Over')
     gaming()
     
 if __name__ == '__main__':
@@ -46,3 +51,36 @@ if __name__ == '__main__':
     
 # https://www.techiedelight.com/find-index-character-python-string/#:~:text=The%20standard%20solution%20to%20find%20a%20character%E2%80%99s%20position,returns%20-1%20when%20the%20character%20is%20not%20found.
 # Enumarate Function
+
+
+# Colab
+word='PERRO'
+blank= str('_'*len(word))
+new_word = list(word)
+
+def findLetter(letter, word):
+  index = [i for i,c in enumerate(word) if c == letter]
+  if len(index) == 0:
+    return -1
+  else:
+    return index[0]
+
+
+
+def changeWord(index, blank, letter):
+  blank = list(blank)
+  if index == -1:
+    return 'Not Found.'
+  else:
+    blank[index] = letter
+    return ''.join(blank)
+
+def separate_blank(string_user_try):
+  return ' '.join(string_user_try)
+
+
+while True:
+
+  option = input('Ingresa una letra ...')
+  user_try = changeWord(findLetter(option.upper(), word), blank, option.upper())
+  print(separate_blank(changeWord(findLetter(option.upper(), word), user_try, option.upper())))
