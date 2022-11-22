e60f47caae635ae646633ea5c50e2798a5d2a22a
gonzakayunawel <go.cayunao@gmail.com>
2022-11-21 23:30:09 -0400

    Modificacion al bucle While

--- game.py
+++ game.py
@@ -1,25 +1,29 @@
-import os, random
+import os
 
 def run():
 
-    word='PERRO'
-    blank= str('_'*len(word))
-    new_word = list(word)
+    word = 'PERRO'
+    blank = str('_'*len(word))
+
+    # Muestra el inicio y limpia la pantalla
 
     def present():
-        os.system("clear")
-        print('Welcome to the HangManGame')
-        print('\nTry to guess the word!')
-        print('\n')
+      os.system("clear")
+      print('Welcome to the HangManGame')
+      print('\nTry to guess the word!')
 
+    # Encuentra la letra indicada en la palabra
+    # Si encuentra la letra devuelve el índice de la letra en la palabra
+    # Si no se encuentra devuelve -1
+    # letter = la letra del usuario
+    # word = la palabra a adivinar
+    
     def findLetter(letter, word):
       index = [i for i,c in enumerate(word) if c == letter]
       if len(index) == 0:
         return -1
       else:
         return index[0]
-
-
 
     def changeWord(index, blank, letter):
       blank = list(blank)
@@ -31,13 +35,21 @@ def run():
 
     def separate_blank(string_user_try):
       return ' '.join(string_user_try)
-
-
+    
+    present()
+    count = 0
+    user_options = []
     while True:
-
-      option = input('Ingresa una letra ...')
-      user_try = changeWord(findLetter(option.upper(), word), blank, option.upper())
-      print(separate_blank(changeWord(findLetter(option.upper(), word), user_try, option.upper())))
+      option = input('Please enter a letter... ')
+      if findLetter(option.upper(), word) == -1:
+        print('Not found.')
+        count += 1
+      elif count == 3:
+        print('You lose')
+        break
+      else:
+        user_options.append(changeWord(findLetter(option.upper(), word), blank, option.upper()))
+        print(user_options)
 
 if __name__ == '__main__':
     run()
