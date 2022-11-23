ecb8414cfb66b673add9541ef3e56085de2c3a62
gonzakayunawel <go.cayunao@gmail.com>
2022-11-22 21:54:19 -0400

    Lógica terminada, juego funcional

--- game.py
+++ game.py
@@ -1,61 +1,66 @@
-import os
+import os, random
 
 def run():
-
-    word = 'PERRO'
-    blank = str('_'*len(word))
-
-    # Muestra el inicio y limpia la pantalla
-
-    def present():
-      os.system("clear")
-      print('Welcome to the HangManGame')
-      print('\nTry to guess the word!')
-
-    # Encuentra la letra indicada en la palabra
-    # Si encuentra la letra devuelve el índice de la letra en la palabra
-    # Si no se encuentra devuelve -1
-    # letter = la letra del usuario
-    # word = la palabra a adivinar
+  words = ['perro', 'gato', 'conejo', 'caballo']
+  def present():
+    os.system('clear')
+    print('Welcome! to the HangManGame')
+    print('\n')
     
-    def findLetter(letter, word):
-      index = [i for i,c in enumerate(word) if c == letter]
-      if len(index) == 0:
-        return -1
+  present()
+  
+  def random_word():
+    word = random.choice(words)
+    return word
+  word = random_word()
+  
+  def game():
+    blank = '_'*len(word)
+    c = 0
+    while c < 3:
+      user_letter = input('Insert a letter... ')
+      print('\n')
+      
+      if word.count(user_letter.lower()) == 1:
+        index = word.rfind(user_letter.lower())
+        blank = list(blank)
+        blank[index] = word[index]
+        blank = blank
+        print(blank)
+        print('\n')
+        print('Yeah. Your letter is in the word!')
+        user_word = ''.join(blank)
+        if user_word == word:
+          break
+      elif word.count(user_letter.lower()) == 2:
+        index = word.rfind(user_letter.lower())
+        blank = list(blank)
+        blank[index] = word[index]
+        blank = blank
+        index2 = word.index(user_letter.lower())
+        blank = list(blank)
+        blank[index2] = word[index2]
+        blank = blank
+        print(blank)
+        print('\n')
+        print('Yeah. Your letter is in the word!')
+        user_word = ''.join(blank)
+        if user_word == word:
+          break
       else:
-        return index[0]
-
-    def changeWord(index, blank, letter):
-      blank = list(blank)
-      if index == -1:
-        return 'Not Found.'
-      else:
-        blank[index] = letter
-        return ''.join(blank)
-
-    def separate_blank(string_user_try):
-      return ' '.join(string_user_try)
-    
-    present()
-    count = 0
-    user_options = []
-    while True:
-      option = input('Please enter a letter... ')
-      if findLetter(option.upper(), word) == -1:
-        print('Not found.')
-        count += 1
-      elif count == 3:
-        print('You lose')
-        break
-      else:
-        user_options.append(changeWord(findLetter(option.upper(), word), blank, option.upper()))
-        print(user_options)
+        c += 1
+        print('Boring, letter not found.')
+  
+    if user_word == word:
+      print('\n')
+      print('You win.')
+      print('Congratulations')
+    elif c == 3:
+      print('\n')
+      print('You lose.')
+      print('The game is over.')
+  
+  game()
 
 if __name__ == '__main__':
     run()
-
-# https://www.techiedelight.com/find-index-character-python-string/#:~:text=The%20standard%20solution%20to%20find%20a%20character%E2%80%99s%20position,returns%20-1%20when%20the%20character%20is%20not%20found.
-# Enumarate Function
-
-
-# Colab
